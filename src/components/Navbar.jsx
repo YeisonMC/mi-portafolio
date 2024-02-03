db4ff2209c74fe9369df153a17d9381f608f5f7b
YeisonMC <yeisonmacalupu01@gmail.com>
2024-02-02 23:13:27 -0500

    modicacion de navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,5 @@
 import React, { useState, useEffect } from "react";
 import "../assets/styles/TipoGrafias.css";
-import HamburgerIcon from "../assets/images/navbar/hamburger.svg";
-import CloseIcon from "../assets/images/navbar/close.svg";
 
 export const Navbar = () => {
   const [isMenuOpen, setMenuOpen] = useState(false);
@@ -34,7 +32,251 @@ export const Navbar = () => {
 
   return (
     <>
-      <nav className="bg-white border-gray-200 dark:bg-gray-900">
+      <nav className="bg-white dark:bg-black">
+        <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 ">
+          <div className="flex items-center justify-between h-16">
+            <div className="flex items-center ">
+              <div className="flex-shrink-0">
+                <a href="#" className="dark:text-white">
+                  Logo
+                </a>
+              </div>
+            </div>
+            <div className="hidden md:block">
+              <div className="ml-4 flex items-center space-x-4">
+                <a
+                  href="#"
+                  className="hover:bg-black hover:text-white dark:text-white dark:hover:bg-white dark:hover:text-black  rounded-lg p-2"
+                >
+                  Inicio
+                </a>
+                <a
+                  href="#proyectos"
+                  className="hover:bg-black hover:text-white dark:text-white dark:hover:bg-white dark:hover:text-black rounded-lg p-2"
+                >
+                  Proyectos
+                </a>
+                <a
+                  href="#habilidades"
+                  className="hover:bg-black hover:text-white dark:text-white dark:hover:bg-white dark:hover:text-black rounded-lg p-2"
+                >
+                  Habilidades
+                </a>
+                <a
+                  href="#contacto"
+                  className="hover:bg-black hover:text-white dark:text-white dark:hover:bg-white dark:hover:text-black rounded-lg p-2"
+                >
+                  Contacto
+                </a>
+                <button
+                  onClick={handleTheme}
+                  className="block py-2 px-3 cursor-pointer"
+                >
+                  {theme === "light" ? (
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      viewBox="0 0 24 24"
+                      fill="currentColor"
+                      className="w-7 h-7 max-sm:w-8 max-sm:h-8"
+                    >
+                      <path
+                        fillRule="evenodd"
+                        d="M9.528 1.718a.75.75 0 0 1 .162.819A8.97 8.97 0 0 0 9 6a9 9 0 0 0 9 9 8.97 8.97 0 0 0 3.463-.69.75.75 0 0 1 .981.98 10.503 10.503 0 0 1-9.694 6.46c-5.799 0-10.5-4.7-10.5-10.5 0-4.368 2.667-8.112 6.46-9.694a.75.75 0 0 1 .818.162Z"
+                        clipRule="evenodd"
+                      />
+                    </svg>
+                  ) : (
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      viewBox="0 0 24 24"
+                      className="w-7 h-7 fill-amber-400 max-sm:w-9 max-sm:h-9"
+                    >
+                      <path d="M12 2.25a.75.75 0 0 1 .75.75v2.25a.75.75 0 0 1-1.5 0V3a.75.75 0 0 1 .75-.75ZM7.5 12a4.5 4.5 0 1 1 9 0 4.5 4.5 0 0 1-9 0ZM18.894 6.166a.75.75 0 0 0-1.06-1.06l-1.591 1.59a.75.75 0 1 0 1.06 1.061l1.591-1.59ZM21.75 12a.75.75 0 0 1-.75.75h-2.25a.75.75 0 0 1 0-1.5H21a.75.75 0 0 1 .75.75ZM17.834 18.894a.75.75 0 0 0 1.06-1.06l-1.59-1.591a.75.75 0 1 0-1.061 1.06l1.59 1.591ZM12 18a.75.75 0 0 1 .75.75V21a.75.75 0 0 1-1.5 0v-2.25A.75.75 0 0 1 12 18ZM7.758 17.303a.75.75 0 0 0-1.061-1.06l-1.591 1.59a.75.75 0 0 0 1.06 1.061l1.591-1.59ZM6 12a.75.75 0 0 1-.75.75H3a.75.75 0 0 1 0-1.5h2.25A.75.75 0 0 1 6 12ZM6.697 7.757a.75.75 0 0 0 1.06-1.06l-1.59-1.591a.75.75 0 0 0-1.061 1.06l1.59 1.591Z" />
+                    </svg>
+                  )}
+                </button>
+              </div>
+            </div>
+            <div className="md:hidden flex items-center">
+              <button
+                className="inline-flex items-center justify-center p-2 rounded-md text-white md:text-white hover:text-white focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white"
+                onClick={toggleMenu}
+              >
+                {isMenuOpen ? (
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    viewBox="0 0 24 24"
+                    className="w-8 h-8 fill-black dark:fill-white"
+                  >
+                    <path
+                      fillRule="evenodd"
+                      d="M5.47 5.47a.75.75 0 0 1 1.06 0L12 10.94l5.47-5.47a.75.75 0 1 1 1.06 1.06L13.06 12l5.47 5.47a.75.75 0 1 1-1.06 1.06L12 13.06l-5.47 5.47a.75.75 0 0 1-1.06-1.06L10.94 12 5.47 6.53a.75.75 0 0 1 0-1.06Z"
+                      clipRule="evenodd"
+                    />
+                  </svg>
+                ) : (
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    viewBox="0 0 24 24"
+                    className="w-8 h-8 fill-black dark:fill-white"
+                  >
+                    <path
+                      fillRule="evenodd"
+                      d="M3 6.75A.75.75 0 0 1 3.75 6h16.5a.75.75 0 0 1 0 1.5H3.75A.75.75 0 0 1 3 6.75ZM3 12a.75.75 0 0 1 .75-.75h16.5a.75.75 0 0 1 0 1.5H3.75A.75.75 0 0 1 3 12Zm0 5.25a.75.75 0 0 1 .75-.75h16.5a.75.75 0 0 1 0 1.5H3.75a.75.75 0 0 1-.75-.75Z"
+                      clipRule="evenodd"
+                    />
+                  </svg>
+                )}
+              </button>
+            </div>
+          </div>
+        </div>
+        {isMenuOpen && (
+          <div className="md:hidden">
+            <div className="px-2 pt-2 pb-3 space-y-1 sm:px-3">
+              <a
+                href="#"
+                className="block hover:bg-black hover:text-white dark:text-white dark:hover:bg-white dark:hover:text-black  rounded-lg p-2"
+              >
+                Inicio
+              </a>
+              <a
+                href="#proyectos"
+                className="block hover:bg-black hover:text-white dark:text-white dark:hover:bg-white dark:hover:text-black rounded-lg p-2"
+              >
+                Proyectos
+              </a>
+              <a
+                href="#habilidades"
+                className="block hover:bg-black hover:text-white dark:text-white dark:hover:bg-white dark:hover:text-black rounded-lg p-2"
+              >
+                Habilidades
+              </a>
+              <a
+                href="#contacto"
+                className="block hover:bg-black hover:text-white dark:text-white dark:hover:bg-white dark:hover:text-black rounded-lg p-2"
+              >
+                Contacto
+              </a>
+              <button
+                onClick={handleTheme}
+                className="block py-2 px-3 cursor-pointer"
+              >
+                {theme === "light" ? (
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    viewBox="0 0 24 24"
+                    className="w-7 h-7 max-sm:w-8 max-sm:h-8"
+                  >
+                    <path
+                      fillRule="evenodd"
+                      d="M9.528 1.718a.75.75 0 0 1 .162.819A8.97 8.97 0 0 0 9 6a9 9 0 0 0 9 9 8.97 8.97 0 0 0 3.463-.69.75.75 0 0 1 .981.98 10.503 10.503 0 0 1-9.694 6.46c-5.799 0-10.5-4.7-10.5-10.5 0-4.368 2.667-8.112 6.46-9.694a.75.75 0 0 1 .818.162Z"
+                      clipRule="evenodd"
+                    />
+                  </svg>
+                ) : (
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    viewBox="0 0 24 24"
+                    className="w-7 h-7 fill-amber-400 max-sm:w-9 max-sm:h-9"
+                  >
+                    <path d="M12 2.25a.75.75 0 0 1 .75.75v2.25a.75.75 0 0 1-1.5 0V3a.75.75 0 0 1 .75-.75ZM7.5 12a4.5 4.5 0 1 1 9 0 4.5 4.5 0 0 1-9 0ZM18.894 6.166a.75.75 0 0 0-1.06-1.06l-1.591 1.59a.75.75 0 1 0 1.06 1.061l1.591-1.59ZM21.75 12a.75.75 0 0 1-.75.75h-2.25a.75.75 0 0 1 0-1.5H21a.75.75 0 0 1 .75.75ZM17.834 18.894a.75.75 0 0 0 1.06-1.06l-1.59-1.591a.75.75 0 1 0-1.061 1.06l1.59 1.591ZM12 18a.75.75 0 0 1 .75.75V21a.75.75 0 0 1-1.5 0v-2.25A.75.75 0 0 1 12 18ZM7.758 17.303a.75.75 0 0 0-1.061-1.06l-1.591 1.59a.75.75 0 0 0 1.06 1.061l1.591-1.59ZM6 12a.75.75 0 0 1-.75.75H3a.75.75 0 0 1 0-1.5h2.25A.75.75 0 0 1 6 12ZM6.697 7.757a.75.75 0 0 0 1.06-1.06l-1.59-1.591a.75.75 0 0 0-1.061 1.06l1.59 1.591Z" />
+                  </svg>
+                )}
+              </button>
+            </div>
+          </div>
+        )}
+      </nav>
+
+      {/* <nav className="bg-white p-4 dark:bg-gray-900">
+        <div className="container mx-auto flex items-center justify-between">
+          <div className="flex items-center">
+            <img
+              src="https://upload.wikimedia.org/wikipedia/commons/thumb/b/b7/Komatsu_company_logos.svg/1024px-Komatsu_company_logos.svg.png"
+              alt="Logo"
+              className="h-12 w-18"
+            />
+          </div>
+
+          <div className="hidden lg:flex space-x-4">
+            <a href="#" className="text-black nav-link dark:text-white">
+              Inicio
+            </a>
+            <a
+              href="#proyectos"
+              className="text-black nav-link dark:text-white"
+            >
+              Proyectos
+            </a>
+            <a href="#" className="text-black nav-link dark:text-white">
+              Habilidades
+            </a>
+            <a href="#" className="text-black nav-link dark:text-white">
+              Contacto
+            </a>
+
+            <label
+              htmlFor="check"
+              className="bg-slate-500 dark:bg-gray-100 cursor-pointer relative w-16 h-8 rounded-full"
+            >
+              <input
+                onClick={handleTheme}
+                type="checkbox"
+                id="check"
+                className="sr-only peer"
+              />
+              <span className="w-1/3 h-3/4 bg-gray-900 absolute rounded-full left-2 top-1 peer-checked:bg-[#DBC602] peer-checked:left-9 transition-all duration-500"></span>
+            </label>
+          </div>
+
+          <div className="lg:hidden transition-all duration-300">
+            <button onClick={toggleMenu} className="  focus:outline-none">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 24 24"
+                fill="currentColor"
+                class="w-6 h-6"
+              >
+                <path
+                  fill-rule="evenodd"
+                  d="M3 6.75A.75.75 0 0 1 3.75 6h16.5a.75.75 0 0 1 0 1.5H3.75A.75.75 0 0 1 3 6.75ZM3 12a.75.75 0 0 1 .75-.75h16.5a.75.75 0 0 1 0 1.5H3.75A.75.75 0 0 1 3 12Zm0 5.25a.75.75 0 0 1 .75-.75h16.5a.75.75 0 0 1 0 1.5H3.75a.75.75 0 0 1-.75-.75Z"
+                  clip-rule="evenodd"
+                />
+              </svg>
+            </button>
+          </div>
+        </div>
+
+        <div
+          className={` lg:hidden ${
+            isMenuOpen ? "block" : "hidden"
+          } bg-gray-800 p-4 `}
+        >
+          <a href="#ola" className="text-white block mb-2">
+            Inicio
+          </a>
+          <a href="#proyectos" className="text-white block mb-2">
+            Proyectos
+          </a>
+          <a href="#" className="text-white block mb-2">
+            Habilidades
+          </a>
+          <a href="#" className="text-white block ">
+            Contacto
+          </a>
+          <div className="">
+            <button
+              className="bg-slate-600 px-4 py-2 rounded hover:bg-slate-300 dark:bg-white dark:text-white dark:hover:bg-slate-800"
+              onClick={handleTheme}
+            >
+              Modo oscuro
+            </button>
+          </div>
+        </div>
+      </nav> */}
+
+      {/* <nav className="bg-white border-gray-200 dark:bg-gray-900">
         <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
           <a className="flex items-center space-x-3 rtl:space-x-reverse">
             <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">
@@ -135,7 +377,7 @@ export const Navbar = () => {
             </ul>
           </div>
         </div>
-      </nav>
+      </nav> */}
     </>
   );
 };
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,13 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./node_modules/flowbite/**/*.js",
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   darkMode: "class",
   theme: {
     extend: {},
   },
-  plugins: [require("flowbite/plugin")],
+  plugins: [],
 };
