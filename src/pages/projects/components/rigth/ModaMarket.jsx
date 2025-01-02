3fdbfb88713bdd5586220079403799974ea0b28f
YeisonMC <yeisonmacalupu01@gmail.com>
2025-01-02 00:42:46 -0500

    Se actualizo el browserList en el package, y se puso el nuevo moda market junto el link de la pagina

--- src/pages/projects/components/rigth/ModaMarket.jsx
+++ src/pages/projects/components/rigth/ModaMarket.jsx
@@ -2,6 +2,7 @@ import React from "react";
 
 //? ICONOS SVG FRONTEND
 import ModaMarketProject from "@projects/moda-market.svg";
+import ModaMarketProjectNow from "@projects/Moda-Market-Now.svg";
 import Reacticon from "@frontend/react.svg";
 import Vite from "@frontend/vite.svg";
 import Tailwind from "@frontend/tailwind.svg";
@@ -45,7 +46,7 @@ const ModaMarket = () => {
                 </svg>
               </a>
               <a
-                href="https://moda-market-frontend-h3f1.vercel.app/"
+                href="https://frontend-modamarket.vercel.app/"
                 target="_blank"
                 className="hover:-translate-y-1 transition duration-300"
               >
@@ -106,31 +107,10 @@ const ModaMarket = () => {
           </p>
         </div>
         <img
-          src={ModaMarketProject}
+          src={ModaMarketProjectNow}
           alt="ModaMarket - Yeissen Beckam"
           className="rounded-md"
         />
-        {/* <div className="group relative max-xl:my-auto shadow-md">
-          <img
-            src={ModaMarketProject}
-            alt="ModaMarket - Yeissen Beckam"
-            className="rounded-md group-hover:grayscale transition duration-500"
-          />
-          <div className="absolute inset-0 flex items-center justify-center opacity-0 group-hover:opacity-100  hover:backdrop-blur-sm hover:bg-[rgba(0,0,0,0.1)] transition-all duration-500 rounded-md">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              viewBox="0 0 24 24"
-              className="w-8 h-8 fill-slate-100 cursor-pointer"
-            >
-              <path d="M12 15a3 3 0 1 0 0-6 3 3 0 0 0 0 6Z" />
-              <path
-                fillRule="evenodd"
-                d="M1.323 11.447C2.811 6.976 7.028 3.75 12.001 3.75c4.97 0 9.185 3.223 10.675 7.69.12.362.12.752 0 1.113-1.487 4.471-5.705 7.697-10.677 7.697-4.97 0-9.186-3.223-10.675-7.69a1.762 1.762 0 0 1 0-1.113ZM17.25 12a5.25 5.25 0 1 1-10.5 0 5.25 5.25 0 0 1 10.5 0Z"
-                clipRule="evenodd"
-              />
-            </svg>
-          </div>
-        </div> */}
       </div>
     </div>
   );
